e1dd34f045760f608e6fa09a11739b192fce2a7b
Miguel Teran <miguel.teranj02@gmail.com>
2026-06-10 00:00:53 -0400

    Use pnpm instead of npm

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -3,4 +3,4 @@ set -e
 DIR="$(cd "$(dirname "$0")/.." && pwd)"
 avahi-publish-service cohete-de-agua _http._tcp 3000 &
 trap "kill $! 2>/dev/null" EXIT
-cd "$DIR" && npm run dev
+cd "$DIR" && pnpm dev
